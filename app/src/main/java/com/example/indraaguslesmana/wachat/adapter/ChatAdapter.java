59a872464c25c68ae311a8ca1541bb17cb8b8999
Indra Agus Lesmana <indra.agus@kiranatama.com>
2017-02-27 07:05:17 +0700

    clean up…

--- app/src/main/java/com/example/indraaguslesmana/wachat/adapter/ChatAdapter.java
+++ app/src/main/java/com/example/indraaguslesmana/wachat/adapter/ChatAdapter.java
@@ -3,7 +3,6 @@ package com.example.indraaguslesmana.wachat.adapter;
 import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -11,7 +10,6 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.example.indraaguslesmana.wachat.R;
 import com.example.indraaguslesmana.wachat.Utility.Helpers;
 import com.example.indraaguslesmana.wachat.Utility.PreferenceUtils;
