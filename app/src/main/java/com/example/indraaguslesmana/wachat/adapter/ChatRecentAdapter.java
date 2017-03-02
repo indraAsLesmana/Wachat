c57d2ebfa3e2c1ba67fb79b041e8caaca6992506
Indra Agus Lesmana <indra.agus@kiranatama.com>
2017-03-02 11:57:02 +0700

    fix view adapter…

--- app/src/main/java/com/example/indraaguslesmana/wachat/adapter/ChatRecentAdapter.java
+++ app/src/main/java/com/example/indraaguslesmana/wachat/adapter/ChatRecentAdapter.java
@@ -6,8 +6,11 @@ import android.support.annotation.NonNull;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.indraaguslesmana.wachat.R;
+import com.example.indraaguslesmana.wachat.Utility.Helpers;
 import com.example.indraaguslesmana.wachat.model.Chat_recent_model;
 
 import java.util.List;
@@ -30,7 +33,23 @@ public class ChatRecentAdapter extends ArrayAdapter<Chat_recent_model> {
                     getLayoutInflater().inflate(R.layout.message_item_history, parent, false);
         }
 
+        TextView recent_chatName = (TextView) convertView.findViewById(R.id.recent_chat_name);
+        TextView recent_chatMessage = (TextView) convertView.findViewById(R.id.recent_chat_message);
+        TextView recent_chatTime = (TextView) convertView.findViewById(R.id.recent_time);
+        ImageView recent_chatProfileImage = (ImageView) convertView.findViewById(R.id.recent_chat_image);
 
+        Chat_recent_model primary_recent_chat = getItem(position);
+
+        if (primary_recent_chat != null) {
+            recent_chatName.setText(primary_recent_chat.getName());
+            recent_chatMessage.setText(primary_recent_chat.getMessage());
+
+            long time = Long.valueOf(primary_recent_chat.getTime_stamp().toString());
+            String timeResult = Helpers.convertUnixTime(time, Helpers.DATE_STYLE_ONLY_TIME);
+
+            recent_chatTime.setText(timeResult);
+            recent_chatProfileImage.setImageResource(R.drawable.user_pacific);
+        }
 
         return convertView;
     }
